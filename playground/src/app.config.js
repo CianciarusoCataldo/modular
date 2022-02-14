b30f1bca33da43fcc360151489a01e910e47235b
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2022-02-14 11:03:28 +0100

    fix

--- playground/src/app.config.js
+++ playground/src/app.config.js
@@ -1,13 +1,11 @@
 import { lazy } from "react";
 
-import App from "app/App";
 import { DrawerContent, DrawerLogo } from "contents/drawer";
 import FooterContent from "contents/footer";
 import HeaderContent from "contents/header";
 
 const appConfig = {
   pagesRendering: (route) => lazy(() => import(`pages/${route}`)),
-  content: App,
   modals: {},
   preloader: () => <div className="preloader" />,
   header: HeaderContent,
